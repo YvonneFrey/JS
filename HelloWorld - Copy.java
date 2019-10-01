1e4e8f953d835f5409a444e5c139143880aea6af
Alarja <angelina.alarja@accenture.com>
2019-10-01 12:30:19 +0200

    Angelina

--- HelloWorld - Copy.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        // Prints "Hello, Flow Ryan" to the terminal window.
-        System.out.println("Hello, Flow Ryan");
-    }
-
-}
--- README - Copy (2).md
+++
@@ -1,3 +0,0 @@
-
-OVERFREDDING
->>>>>>> 6941fda5bc1e4b72d9df6a579ffe9e9c0635d7d6
